40d6edd47b584c86d8c884a539780becfd640109
Vladislav Kindushov(Darcvizer) <Darcvizer@gmail.com>
2020-04-10 17:14:18 +0300

    Update Auto-Delete-for_Blender28.py

--- Auto-Delete-for_Blender28.py
+++ Auto-Delete-for_Blender28.py
@@ -1,5 +1,5 @@
 import bpy
-import rna_keymap_ui
+
 bl_info = {
 	"name": "Auto Delete :)",
 	"location": "View3D > Add > Mesh > Auto Delete,",
@@ -21,12 +21,12 @@ def find_connected_verts(me, found_index):
 	return len(connecting_edges)
 
 
-class AutoDelete(bpy.types.Operator):
+class AutoDelete_OT_darcvizer(bpy.types.Operator):
 	""" Dissolves mesh elements based on context instead
 	of forcing the user to select from a menu what
 	it should dissolve.
 	"""
-	bl_idname = "view3d.autodelete"
+	bl_idname = "mesh.autodelete_ot_darcvizer"
 	bl_label = "Auto Delete"
 	bl_options = {'UNDO'}
 
@@ -96,108 +96,50 @@ class AutoDelete(bpy.types.Operator):
 
 # bpy.utils.register_class(MeshDissolveContextual_OT_vlad)
 
-
-addon_keymaps = []
-
-
-def get_addon_preferences():
-	''' quick wrapper for referencing addon preferences '''
-	addon_preferences = bpy.context.user_preferences.addons[__name__].preferences
-	return addon_preferences
+classes = (AutoDelete_OT_darcvizer,)
 
 
-def get_hotkey_entry_item(km, kmi_value):
-	'''
-	returns hotkey of specific type, with specific properties.name (keymap is not a dict, so referencing by keys is not enough
-	if there are multiple hotkeys!)
-	'''
-	for i, km_item in enumerate(km.keymap_items):
-		if km.keymap_items[i].idname == kmi_value:
-			return km_item
-	return None
-
-
-def add_hotkey():
-	user_preferences = bpy.context.preferences
-	addon_prefs = user_preferences.addons[__name__].preferences
-
-	wm = bpy.context.window_manager
-	kc = wm.keyconfigs.addon
-	km = kc.keymaps.new(name="3D View Generic", space_type='VIEW_3D', region_type='WINDOW')
-	kmi = km.keymap_items.new(AutoDelete.bl_idname, 'X', 'PRESS', shift=False, ctrl=False, alt=False)
-	# kmi.properties.name = "view3d.advancedmove"
-	kmi.active = True
-	addon_keymaps.append((km, kmi))
-
-
-class AutoDelete_Add_Hotkey(bpy.types.Operator):
-	''' Add hotkey entry '''
-	bl_idname = "auto_delete.add_hotkey"
-	bl_label = "Auto Delete Add Hotkey"
-	bl_options = {'REGISTER', 'INTERNAL'}
-
-	def execute(self, context):
-		add_hotkey()
-
-		self.report({'INFO'}, "Hotkey added in User Preferences -> Input -> Screen -> Screen (Global)")
-		return {'FINISHED'}
-
-
-def remove_hotkey():
-	''' clears all addon level keymap hotkeys stored in addon_keymaps '''
-	wm = bpy.context.window_manager
-	kc = wm.keyconfigs.user
-	km = kc.keymaps['3D View Generic']
-
-	for i in bpy.context.window_manager.keyconfigs.addon.keymaps['3D View Generic'].keymap_items:
-		if i.name == 'Auto Delete' or i.name == 'VIEW3D_OT_auto_delete':
-			bpy.context.window_manager.keyconfigs.addon.keymaps['3D View Generic'].keymap_items.remove(i)
-
-
-class StreamExtrudePref(bpy.types.AddonPreferences):
-	bl_idname = __name__
-
-	def draw(self, context):
-		layout = self.layout
-		# ---------------------------------
-		box = layout.box()
-		split = box.split()
-		col = split.column()
-		# col.label("Setup Advanced Move")
-		col.separator()
-		wm = bpy.context.window_manager
-		kc = wm.keyconfigs.user
-		km = kc.keymaps['3D View Generic']
-		kmi = get_hotkey_entry_item(km, "view3d.autodelete")
-		if kmi:
-			col.context_pointer_set("keymap", km)
-			rna_keymap_ui.draw_kmi([], kc, km, kmi, col, 0)
-		else:
-			col.label("No hotkey entry found")
-			col.operator(AutoDelete_Add_Hotkey.bl_idname, text="Add hotkey entry", icon='ZOOMIN')
-
-classes = (AutoDelete, AutoDelete_Add_Hotkey, StreamExtrudePref)
-
-def CreateHotkey():
-	global keys
-	wm = bpy.context.window_manager
-	addon_keyconfig = wm.keyconfigs.addon
-	kc = addon_keyconfig
-	km = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
-	kmi = km.keymap_items.new(idname='view3d.advancedmove', type='K', value='PRESS', alt=True)
-	keys.append((km, kmi))
-	
 def register():
 	for c in classes:
 		bpy.utils.register_class(c)
-	CreateHotkey()
+	global obj
+	global mesh
+	activConfig = bpy.context.window_manager.keyconfigs.active.name
+	wm = bpy.context.window_manager
+	#keymaps_3DV = wm.keyconfigs[activConfig].keymaps['Object Mode']
+	#keymap = keymaps_3DV.keymap_items['object.delete']
+	#obj = keymap.idname
+	#keymap.idname = 'mesh.autodelete_ot_darcvizer'
+	#keymaps_3DV = wm.keyconfigs[activConfig].keymaps['Mesh']
+
+
+
+
+	# i = 0
+	# while (True):
+	# 	#print(bpy.context.window_manager.keyconfigs[activConfig].keymaps['Mesh'].keymap_items[i].type)
+	# 	if bpy.context.window_manager.keyconfigs[activConfig].keymaps['Mesh'].keymap_items[i].type == 'X':
+	# 		mesh = bpy.context.window_manager.keyconfigs[activConfig].keymaps['Mesh'].keymap_items[i].idname
+	# 		bpy.context.window_manager.keyconfigs[activConfig].keymaps['Mesh'].keymap_items[i].idname = 'mesh.autodelete_ot_darcvizer'
+	# 		break
+	# 	else:
+	# 		i = i + 1
+
 
 
 def unregister():
 	for c in reversed(classes):
 		bpy.utils.unregister_class(c)
-	for km, kmi in keys:
-		km.keymap_items.remove(kmi)
+
+
+	global obj
+	global mesh
+	wm = bpy.context.window_manager
+	keymaps_3DV = wm.keyconfigs[0].keymaps['Object Mode']
+	keymap = keymaps_3DV.keymap_items['mesh.autodelete_ot_darcvizer']
+	keymap.idname = obj
+	keymaps = wm.keyconfigs[0].keymaps['Mesh']
+	keymaps.keymap_items['mesh.autodelete_ot_darcvizer']
+	keymap.idname = mesh
 if __name__ == "__main__":
 	register()
-
